ccac903743d538c19f4db0bab131f6df04c15c0b
Muhammed Galib Uludag <m231@tnctr.com>
2023-02-22 23:40:22 +0100

    Update enum_sequence.hpp

--- enum_sequence.hpp
+++ enum_sequence.hpp
@@ -5,21 +5,17 @@
 namespace mgutility{
 namespace detail{
 
-#ifndef MIN_ENUM_VALUE
-#define MIN_ENUM_VALUE 0
-#endif
-
 template <typename Enum, Enum ...>
 struct enum_sequence{};
 
-template <typename Enum, int N, int ... Next>
-struct enum_sequence_helper : enum_sequence_helper<Enum, N-1, N-1, Next...>{};
+template <typename Enum, int Min, int Max, int ... Next>
+struct enum_sequence_helper : enum_sequence_helper<Enum, Min, Max-1, Max-1, Next...>{};
 
-template <typename Enum, int ... Next>
-struct enum_sequence_helper<Enum, MIN_ENUM_VALUE, Next ... >{ using type = enum_sequence<Enum, static_cast<Enum>(Next) ... >; };
+template <typename Enum, int Min, int ... Next>
+struct enum_sequence_helper<Enum, Min, Min, Next ... >{ using type = enum_sequence<Enum, static_cast<Enum>(Next) ... >; };
 
-template <typename Enum, int N>
-using make_enum_sequence = typename enum_sequence_helper<Enum, N>::type;
+template <typename Enum, int Min, int Max>
+using make_enum_sequence = typename enum_sequence_helper<Enum, Min, Max>::type;
 }
 }
 #endif // MGUTILITY_ENUM_SEQ_HPP
